4bf177a527abc804deb4e71425c0955839fdfcdd
vadim3000 <vadimgolban3000@gmail.com>
2021-09-14 18:00:40 +0300

    added styles and content info

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -17,4 +17,13 @@
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque facilis sit voluptas? Ad eos iste
         nesciunt nostrum perferendis, sequi. Deleniti enim eveniet, id nam natus neque temporibus
         veritatis. Necessitatibus, sunt.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque facilis sit voluptas? Ad eos iste
+        nesciunt nostrum perferendis, sequi. Deleniti enim eveniet, id nam natus neque temporibus
+        veritatis. Necessitatibus, sunt.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque facilis sit voluptas? Ad eos iste
+        nesciunt nostrum perferendis, sequi. Deleniti enim eveniet, id nam natus neque temporibus
+        veritatis. Necessitatibus, sunt.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque facilis sit voluptas? Ad eos iste
+        nesciunt nostrum perferendis, sequi. Deleniti enim eveniet, id nam natus neque temporibus
+        veritatis. Necessitatibus, sunt.</p>
 @endsection

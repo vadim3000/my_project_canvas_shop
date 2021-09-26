4850d8ed11318b242c3502ddec99ad7c9a7bb2cd
vadim3000 <vadimgolban3000@gmail.com>
2021-09-26 17:29:06 +0300

    added Dummy logger functionalitaty though App/ServiceProvider

--- app/Http/Middleware/RequestLoggingMiddleware.php
+++ app/Http/Middleware/RequestLoggingMiddleware.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Middleware;
 
+use App\Services\Logging\RequestLoggerInterface;
 use Illuminate\Http\Request;
-
 
 class RequestLoggingMiddleware
 {
+    private RequestLoggerInterface $logger;
+
+    public function __construct(RequestLoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
 
     public function handle(Request $request, \Closure $next)
     {
-        \Log::info('test');
+        $this->logger->logRequest($request);
 
         return $next($request);
     }
